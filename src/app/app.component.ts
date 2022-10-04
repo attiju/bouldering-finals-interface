e58bd94e5ba9f6950f641012378b6e33e38cd73b
Jules ATTIVISSIMO <jules.attivissimo@gmail.com>
2022-10-04 11:51:02 +0200

    Fix coding style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  `,
-  styles: []
+    selector: 'app-root',
+    template: `
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    `,
+    styles: []
 })
 export class AppComponent {
-  title = 'BoulderingFinalsOverlay';
+    title = 'BoulderingFinalsOverlay';
 }
