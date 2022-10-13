bd387b5e63f91909fc2b393e7aad9c8416538215
Jules ATTIVISSIMO <jules.attivissimo@gmail.com>
2022-10-13 14:01:46 +0200

    add minimal filters

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,17 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {LeaderboardComponent} from './components/leaderboard/leaderboard.component';
 
 
 @NgModule({
     declarations: [
-        LeaderboardComponent
     ],
     imports: [
         CommonModule
     ],
     exports: [
-        LeaderboardComponent
     ]
 })
 export class SharedModule {
